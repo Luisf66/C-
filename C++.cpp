b268689105cb68ff28f0cbe2907793ba53cc81e6
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-16 16:54:09 -0300

    cout

---
+++ C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string texto = "Criando tipos e valores...";
+    int valor_1 = 3;
+    double valor_2 = 6.92;
+    char caractere = 'A';
+    bool valor_3 = false;
+    cout << texto << endl;
+    cout << valor_1 << endl;
+    cout << valor_2 << endl;
+    cout << valor_3 << endl;
+    cout << caractere << endl;
+    return 0;
+}
