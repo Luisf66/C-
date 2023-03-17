224491a11635f6f606f849c422b20e55a40952ac
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-16 21:12:34 -0300

    função cin

--- C++.cpp
+++ C++.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 int main(){
     string texto = "Criando tipos e valores...";
-    int valor_1 = 3;
-    double valor_2 = 6.92;
-    char caractere = 'A';
-    bool valor_3 = false;
-    cout << texto << endl;
-    cout << valor_1 << endl;
-    cout << valor_2 << endl;
-    cout << valor_3 << endl;
-    cout << caractere << endl;
+    cout << "Digite os valores para o calculo" << endl;
+    int x;
+    int y;
+    cin >> x;
+    cin >> y;
+    int soma = x + y;
+    cout << "Resultado:" << soma << endl;
     return 0;
 }
