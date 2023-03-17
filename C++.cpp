eaa45900037537d9a0a50b4ad7a1750e06031196
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-17 14:29:53 -0300

    if ternario

--- C++.cpp
+++ C++.cpp
@@ -7,10 +7,7 @@ int main(){
     int numero_2;
     cin >> numero_1;
     cin >> numero_2;
-    if ( numero_1 > numero_2){
-        cout << "O número 1 é maior que o 2" << endl;
-    } else {
-        cout << "O número 1 não é maior que o 2" << endl;
-    }
+    string resultado = ( numero_1 > numero_2) ? "Número 1 é maior" : "Número 1 não é maior";
+    cout << resultado << endl;
     return 0;
 }
