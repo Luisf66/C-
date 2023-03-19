c95662a47b4af1b5fbbfe66ffb377f7fa6761f26
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-19 11:10:24 -0300

    operações entre números

--- C++.cpp
+++ C++.cpp
@@ -4,16 +4,42 @@ using namespace std;
 
 int main(){
     int numero_1;
+    int numero_2;
+    int operacao;
+    cout << "1 - Soma" << endl;
+    cout << "2 - Subtração" << endl;
+    cout << "3 - Divisão" << endl;
+    cout << "4 - Multiplicação" << endl;
+    cout << "Digite o número 1:" << endl;
     cin >> numero_1;
-    switch (numero_1){
-        case 88:
-            cout << "Maior" << endl;
-            break;
-        case 86:
-            cout << "Menor" << endl;
-            break;
-        default:
-            cout << "Impossivel comparar" << endl;
+    cout << "Digite o número 2:" << endl;
+    cin >> numero_2;
+    cout << "Digite a operação:" << endl;
+    cin >> operacao;
+    int soma;
+    int subtracao;
+    double divisao;
+    double multiplicacao;
+    switch (operacao){
+    case 1:
+        soma = numero_1 + numero_2;
+        cout << "Soma = " << soma << endl;
+        break;
+    case 2:
+        subtracao = numero_1 - numero_2;
+        cout << "subtracao = " << subtracao << endl;
+        break;
+    case 3:
+        divisao = numero_1 / numero_2;
+        cout << "divisao = " << divisao << endl;
+        break;
+    case 4:
+        multiplicacao = numero_1 * numero_2;
+        cout << "multiplicacao = " << multiplicacao << endl;
+        break;
+    default:
+        cout << "Operação inválida" << endl;
+        break;
     }
     return 0;
 }
