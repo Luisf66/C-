2d5a7f7015bcd10a4b969b500c4d9519221dee40
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-20 20:32:12 -0300

    Teste de do-while e switch

--- C++.cpp
+++ C++.cpp
@@ -3,43 +3,29 @@
 using namespace std;
 
 int main(){
-    int numero_1;
-    int numero_2;
-    int operacao;
-    cout << "1 - Soma" << endl;
-    cout << "2 - Subtração" << endl;
-    cout << "3 - Divisão" << endl;
-    cout << "4 - Multiplicação" << endl;
-    cout << "Digite o número 1:" << endl;
-    cin >> numero_1;
-    cout << "Digite o número 2:" << endl;
-    cin >> numero_2;
-    cout << "Digite a operação:" << endl;
-    cin >> operacao;
-    int soma;
-    int subtracao;
-    double divisao;
-    double multiplicacao;
-    switch (operacao){
-    case 1:
-        soma = numero_1 + numero_2;
-        cout << "Soma = " << soma << endl;
-        break;
-    case 2:
-        subtracao = numero_1 - numero_2;
-        cout << "subtracao = " << subtracao << endl;
-        break;
-    case 3:
-        divisao = numero_1 / numero_2;
-        cout << "divisao = " << divisao << endl;
-        break;
-    case 4:
-        multiplicacao = numero_1 * numero_2;
-        cout << "multiplicacao = " << multiplicacao << endl;
-        break;
-    default:
-        cout << "Operação inválida" << endl;
-        break;
-    }
+    string operacoes[4] = {"Adição","Subtração","Divisão","Multiplicação"};
+    int numero;
+    do{
+        cout << "Digite um número de 0 a 3:" << endl;
+        cin >> numero;
+        switch (numero)
+        {
+        case 0:
+            cout << operacoes[0] << endl;
+            break;
+        case 1:
+            cout << operacoes[1] << endl;
+            break;
+        case 2:
+            cout << operacoes[2] << endl;
+            break;
+        case 3:
+            cout << operacoes[3] << endl;
+            break;
+        default:
+            cout << "Número inválido" << endl;
+            break;
+        }
+    } while (numero != 5);
     return 0;
 }
