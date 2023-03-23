b3ce33c6fb87986e9562593f764e68287682b39a
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-23 20:23:17 -0300

    construtor com parametros

--- C++.cpp
+++ C++.cpp
@@ -8,32 +8,28 @@ class Carro {
         string cor;
         double peso;
         double preco;
-            Carro() {
+            Carro(string n, string c, double p, double r) {
                 cout << "\nVeículos disponíveis:" << endl;
+                nome = n;
+                cor = c;
+                peso = p;
+                preco = r;
             }
 };
 
 int main(){
-    Carro fiat;
-        fiat.nome = "Palio";
-        fiat.cor = "Cinza";
-        fiat.peso = 748.9;
-        fiat.preco = 20000;
+    Carro carro1("Palio","Cinza",689.5,20000);
+    
+    cout << "Nome do carro:"<< carro1.nome << endl;
+    cout << "Cor do carro:"<< carro1.cor << endl;
+    cout << "Peso do carro:"<< carro1.peso << endl;
+    cout << "Preço do carro:"<< carro1.preco << endl;
 
-    cout << "Nome do carro:"<< fiat.nome << endl;
-    cout << "Cor do carro:"<< fiat.cor << endl;
-    cout << "Peso do carro:"<< fiat.peso << endl;
-    cout << "Preço do carro:"<< fiat.preco << endl;
-
-    Carro chevrolet;
-        chevrolet.nome = "Tracker";
-        chevrolet.cor = "Branco";
-        chevrolet.peso = 1204.6;
-        chevrolet.preco = 150000;
-
-    cout << "Nome do carro:"<< chevrolet.nome << endl;
-    cout << "Cor do carro:"<< chevrolet.cor << endl;
-    cout << "Peso do carro:"<< chevrolet.peso << endl;
-    cout << "Preço do carro:"<< chevrolet.preco << endl;
+    Carro carro2("Tracker","Branco",1247.8,150000);
+    
+    cout << "Nome do carro:"<< carro2.nome << endl;
+    cout << "Cor do carro:"<< carro2.cor << endl;
+    cout << "Peso do carro:"<< carro2.peso << endl;
+    cout << "Preço do carro:"<< carro2.preco << endl;
     return 0;
 }
