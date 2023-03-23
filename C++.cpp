89cf1f65652fbaa11002bcb73c0b719b2e436419
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-23 14:15:43 -0300

    classes

--- C++.cpp
+++ C++.cpp
@@ -2,34 +2,21 @@
 #include <string>
 using namespace std;
 
-int soma (int numero_1, int numero_2){
-    return numero_1 + numero_2;
-}
-
-int subtracao (int numero_1,int numero_2){
-    if (numero_1 > numero_2){
-        return numero_1 - numero_2;
-    }
-    else{
-        return numero_2 - numero_1;
-    }
-}
+class Carro {
+    public:
+    string nome;
+    string cor;
+    double peso;
+};
 
 int main(){
-    int numero_1;
-    int numero_2;
-    string operacao;
-    cout << "Digite a operação (A = Adição// S = Subtração):" << endl;
-    cin >> operacao;
-    cout << "Digite o número 1:" << endl;
-    cin >> numero_1;
-    cout << "Digite o número 2:" << endl;
-    cin >> numero_2;
-    if (operacao == "A"){
-        cout << soma(numero_1,numero_2);
-    }
-    else{
-        cout << subtracao(numero_1,numero_2);
-    }
+    Carro fiat;
+    fiat.nome = "Palio";
+    fiat.cor = "Cinza";
+    fiat.peso = 748.9;
+
+    cout << "Nome do carro:"<< fiat.nome << endl;
+    cout << "Cor do carro:"<< fiat.cor << endl;
+    cout << "Peso do carro:"<< fiat.peso << endl;
     return 0;
 }
