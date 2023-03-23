7e12f32d1ad411bc73b5b05f08142ac54f38dc88
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-23 14:20:21 -0300

    multiplos objetos

--- C++.cpp
+++ C++.cpp
@@ -18,5 +18,14 @@ int main(){
     cout << "Nome do carro:"<< fiat.nome << endl;
     cout << "Cor do carro:"<< fiat.cor << endl;
     cout << "Peso do carro:"<< fiat.peso << endl;
+
+    Carro chevrolet;
+    chevrolet.nome = "Tracker";
+    chevrolet.cor = "Branco";
+    chevrolet.peso = 1204.6;
+
+    cout << "Nome do carro:"<< chevrolet.nome << endl;
+    cout << "Cor do carro:"<< chevrolet.cor << endl;
+    cout << "Peso do carro:"<< chevrolet.peso << endl;
     return 0;
 }
