7d1eae79fc1699993433b79b47c6f705069e3fb2
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-23 20:08:34 -0300

    métodos

--- C++.cpp
+++ C++.cpp
@@ -7,25 +7,35 @@ class Carro {
     string nome;
     string cor;
     double peso;
+    double preco;
+    void mensagem(){
+        cout << "\nCarros disponíveis:" << endl;
+    }
 };
 
 int main(){
     Carro fiat;
+    fiat.mensagem();
     fiat.nome = "Palio";
     fiat.cor = "Cinza";
     fiat.peso = 748.9;
+    fiat.preco = 20000;
 
     cout << "Nome do carro:"<< fiat.nome << endl;
     cout << "Cor do carro:"<< fiat.cor << endl;
     cout << "Peso do carro:"<< fiat.peso << endl;
+    cout << "Preço do carro:"<< fiat.preco << endl;
 
     Carro chevrolet;
+    chevrolet.mensagem();
     chevrolet.nome = "Tracker";
     chevrolet.cor = "Branco";
     chevrolet.peso = 1204.6;
+    chevrolet.preco = 150000;
 
     cout << "Nome do carro:"<< chevrolet.nome << endl;
     cout << "Cor do carro:"<< chevrolet.cor << endl;
     cout << "Peso do carro:"<< chevrolet.peso << endl;
+    cout << "Preço do carro:"<< chevrolet.preco << endl;
     return 0;
 }
