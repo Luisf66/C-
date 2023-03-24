8fa54c0b7f1c2e5f81acbaf2cee3671a15515f12
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-24 13:40:02 -0300

    atributos dinamicos

--- C++.cpp
+++ C++.cpp
@@ -18,15 +18,26 @@ class Carro {
 };
 
 int main(){
-    Carro carro1("Palio","Cinza",689.5,20000);
-    
+    string nome;
+    string cor;
+    double peso;
+    double preco;
+    cout << "Digite o nome do veículo:" << endl;
+    cin >> nome;
+    cout << "Digite a cor do veículo:" << endl;
+    cin >> cor;
+    cout << "Digite o peso do veículo:" << endl;
+    cin >> peso;
+    cout << "Digite o valor do veículo:" << endl;
+    cin >> preco;
+
+    Carro carro1(nome,cor,peso,preco);
     cout << "Nome do carro:"<< carro1.nome << endl;
     cout << "Cor do carro:"<< carro1.cor << endl;
     cout << "Peso do carro:"<< carro1.peso << endl;
     cout << "Preço do carro:"<< carro1.preco << endl;
 
     Carro carro2("Tracker","Branco",1247.8,150000);
-    
     cout << "Nome do carro:"<< carro2.nome << endl;
     cout << "Cor do carro:"<< carro2.cor << endl;
     cout << "Peso do carro:"<< carro2.peso << endl;
