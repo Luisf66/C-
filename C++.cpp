e144a888549f44b72448b8ad421a64e78af3ebf0
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-26 20:34:10 -0300

    calculadora de IMC

--- C++.cpp
+++ C++.cpp
@@ -2,45 +2,29 @@
 #include <string>
 using namespace std;
 
-class Carro {
+class IMC {
     public:
-        string nome;
-        string cor;
         double peso;
-        double preco;
-            Carro(string n, string c, double p, double r) {
-                cout << "\nVeículos disponíveis:" << endl;
-                nome = n;
-                cor = c;
-                peso = p;
-                preco = r;
-            }
+        double altura;
+        IMC() {
+            cout << "Calculo de IMC" << endl;
+        }
 };
 
+double calcula_imc(double peso,double altura){
+    double imc;
+    imc = peso / (altura*altura);
+    cout << "IMC = " << imc << endl;
+    return imc;
+}
+
+
 int main(){
-    string nome;
-    string cor;
-    double peso;
-    double preco;
-    cout << "Digite o nome do veículo:" << endl;
-    cin >> nome;
-    cout << "Digite a cor do veículo:" << endl;
-    cin >> cor;
-    cout << "Digite o peso do veículo:" << endl;
-    cin >> peso;
-    cout << "Digite o valor do veículo:" << endl;
-    cin >> preco;
-
-    Carro carro1(nome,cor,peso,preco);
-    cout << "Nome do carro:"<< carro1.nome << endl;
-    cout << "Cor do carro:"<< carro1.cor << endl;
-    cout << "Peso do carro:"<< carro1.peso << endl;
-    cout << "Preço do carro:"<< carro1.preco << endl;
-
-    Carro carro2("Tracker","Branco",1247.8,150000);
-    cout << "Nome do carro:"<< carro2.nome << endl;
-    cout << "Cor do carro:"<< carro2.cor << endl;
-    cout << "Peso do carro:"<< carro2.peso << endl;
-    cout << "Preço do carro:"<< carro2.preco << endl;
+    IMC calculo_1;
+    cout << "Digite o seu peso:" << endl;
+    cin >> calculo_1.peso;
+    cout << "Digite a sua altura:" << endl;
+    cin >> calculo_1.altura;
+    calcula_imc(calculo_1.peso,calculo_1.altura);
     return 0;
 }
