eaca88dd4fa5db5e6b3b90f03e53137237d70a08
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-03-26 20:43:03 -0300

    Calculadora de IMC

--- C++.cpp
+++ C++.cpp
@@ -15,6 +15,27 @@ double calcula_imc(double peso,double altura){
     double imc;
     imc = peso / (altura*altura);
     cout << "IMC = " << imc << endl;
+    if (imc < 18.5){
+        cout << "Abaixo do peso " << endl;
+    }
+    else if (imc > 18.5 && imc < 24.9){
+        cout << "Peso ideal " << endl;
+    }
+    else if (imc > 25 && imc < 29.9){
+        cout << "Levemente acima do peso " << endl;
+    }
+    else if (imc > 30 && imc < 34.9){
+        cout << "Obesidade brau I " << endl;
+    }
+    else if (imc > 35 && imc < 39.9){
+        cout << "Obesidade brau II " << endl;
+    }
+    else if (imc > 40){
+        cout << "Obesidade brau III " << endl;
+    }
+    else{
+        cout << "Calculo não realizado...\n" << endl;
+    }
     return imc;
 }
 
