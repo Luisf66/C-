66775d35dfc0f909259db6efcfcbca122e1f9313
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-04-22 20:29:19 -0300

    função gerar numb aleatorio

--- C++.cpp
+++ C++.cpp
@@ -1,51 +1,23 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <random>
+#include <chrono>
 using namespace std;
 
-class IMC {
-    public:
-        double peso;
-        double altura;
-        IMC() {
-            cout << "Calculo de IMC" << endl;
-        }
-};
-
-double calcula_imc(double peso,double altura){
-    double imc;
-    imc = peso / (altura*altura);
-    cout << "IMC = " << imc << endl;
-    if (imc < 18.5){
-        cout << "Abaixo do peso " << endl;
+int gerador_aleatorio(){
+    auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
+    mt19937 gen(seed);
+    uniform_int_distribution<int> dis(1, 50);
+    int i = 1;
+    while (i<=5){
+    int aleatorio = dis(gen);
+    cout << "O número aleatório gerado é: " << aleatorio << endl;
+    i++;
     }
-    else if (imc > 18.5 && imc < 24.9){
-        cout << "Peso ideal " << endl;
-    }
-    else if (imc > 25 && imc < 29.9){
-        cout << "Levemente acima do peso " << endl;
-    }
-    else if (imc > 30 && imc < 34.9){
-        cout << "Obesidade brau I " << endl;
-    }
-    else if (imc > 35 && imc < 39.9){
-        cout << "Obesidade brau II " << endl;
-    }
-    else if (imc > 40){
-        cout << "Obesidade brau III " << endl;
-    }
-    else{
-        cout << "Calculo não realizado...\n" << endl;
-    }
-    return imc;
+    return 0;
 }
 
-
-int main(){
-    IMC calculo_1;
-    cout << "Digite o seu peso:" << endl;
-    cin >> calculo_1.peso;
-    cout << "Digite a sua altura:" << endl;
-    cin >> calculo_1.altura;
-    calcula_imc(calculo_1.peso,calculo_1.altura);
-    return 0;
+int main() {
+    gerador_aleatorio();
 }
