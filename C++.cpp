b25057da43245b9b035baf6e264403f9e5d2db6d
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-04-26 09:33:33 -0300

    gerador de senha com vogal,consoante e numero

--- C++.cpp
+++ C++.cpp
@@ -4,17 +4,31 @@
 #include <random>
 #include <chrono>
 using namespace std;
+//
+// variavel universal : senha
 string senha;
-char colecao[] = {'a','e','i','o','u'};
+//
+// Vetor com componentes da senha
+char vogal[] = {'a','e','i','o','u'};
+char consoantes[] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+char numero[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+//
+//gerador de senha 
 int gerador_aleatorio(){
     auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
     mt19937 gen(seed);
-    uniform_int_distribution<int> dis(0, 4);
+    uniform_int_distribution<int> v(0, 4);
+    uniform_int_distribution<int> c(0, 20);
+    uniform_int_distribution<int> n(0, 9);
     int i = 1;
-    while (i<=5){
-    int aleatorio = dis(gen);
-    senha = senha + colecao[aleatorio];
-    i++;
+    while (i<=4){
+        int aleatorio_v = v(gen);
+        int aleatorio_c = c(gen);
+        int aleatorio_n = n(gen);
+        senha = senha + vogal[aleatorio_v];
+        senha = senha + consoantes[aleatorio_c];
+        senha = senha + numero[aleatorio_n];
+        i++;
     }
     return 0;
 }
