e5eff6e58cfdf28176b25f927dbc6ca741df60a2
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-04-26 09:57:49 -0300

    gerador melhorado

--- C++.cpp
+++ C++.cpp
@@ -15,23 +15,35 @@ char numero[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 //
 //gerador de senha 
 int gerador_aleatorio(){
+    int i = 1;
+    int digitos;
     auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
     mt19937 gen(seed);
     uniform_int_distribution<int> v(0, 4);
     uniform_int_distribution<int> c(0, 20);
     uniform_int_distribution<int> n(0, 9);
-    int i = 1;
-    while (i<=4){
+    cout << "Como Usar O Programa:" << endl;
+    cout << "______________________________" << endl; 
+    cout << "|                            |" << endl; 
+    cout << "| Digitos = 1 : Tamanho = 3  |" << endl; 
+    cout << "| Digitos = 2 : Tamanho = 6  |" << endl;  
+    cout << "| Digitos = 3 : Tamanho = 9  |" << endl;  
+    cout << "| Digitos = 4 : Tamanho = 12 |" << endl;  
+    cout << "| Digitos = 5 : Tamanho = 15 |" << endl;  
+    cout << "|____________________________|" << endl; 
+    cout << "Insira a quantidade de digitos desejada na senha:" << endl;
+    cin >> digitos;
+    //scanf("%i", digitos);
+    while (i<= digitos){
         int aleatorio_v = v(gen);
         int aleatorio_c = c(gen);
         int aleatorio_n = n(gen);
-        senha = senha + vogal[aleatorio_v];
-        senha = senha + consoantes[aleatorio_c];
-        senha = senha + numero[aleatorio_n];
+        senha = senha + vogal[aleatorio_v]+ consoantes[aleatorio_c]+ numero[aleatorio_n];
         i++;
     }
     return 0;
 }
+
 
 int main() {
     gerador_aleatorio();
