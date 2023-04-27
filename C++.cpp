3d070b3ba1d8a01536b99d74deb87d3612ed81d5
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-04-27 20:52:26 -0300

    gerenciamento financeiro com funções básicas

--- C++.cpp
+++ C++.cpp
@@ -2,3 +2,47 @@
 #include <string>
 #include <fstream>
 using namespace std;
+
+//classe contendo informações do usuário
+class MeuMes {
+    double renda;
+    double gasto;
+    int escolher_mes;
+    double saldo;
+};
+//cadastro do dinheiro ganho por mês
+double cadastrar_renda(double renda){
+    cout << "Dinheiro ganho no mês:" << endl;
+    cin >> renda;
+    cout << "Renda mensal:" << renda << endl;
+    return renda;
+}
+//cadastro do dinheiro gasto por mês
+double cadastrar_gasto(double gasto){
+    cout << "Dinheiro gasto no mês:" << endl;
+    cin >> gasto;
+    cout << "Gasto mensal:" << gasto << endl;
+    return gasto;
+}
+//escolha do mês
+int funcao_mes(int escolher_mes){
+    cout << "Informe o mês" << endl;
+    cin >> escolher_mes;
+    cout << "Mês escolhido:" << escolher_mes << endl;
+    return escolher_mes;
+}
+//calculo do saldo do usuário naquele mês
+double calcular_saldo(int renda,int gasto,double saldo){
+    saldo = renda - gasto;
+    cout << "Seu saldo mensal é de:" << saldo << endl;
+    return saldo;
+}
+
+//função principal
+int main(){
+    MeuMes meumes;
+    int mes = funcao_mes(mes);
+    double renda = cadastrar_renda(renda);
+    double gasto = cadastrar_gasto(gasto);
+    double saldo = calcular_saldo(renda,gasto,saldo);
+}
