6987a8450077c04b6abbf40df8cddae56572e06e
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-04-27 21:06:26 -0300

    correção de erro no saldo = 0

--- C++.cpp
+++ C++.cpp
@@ -14,35 +14,38 @@ class MeuMes {
 double cadastrar_renda(double renda){
     cout << "Dinheiro ganho no mês:" << endl;
     cin >> renda;
-    cout << "Renda mensal:" << renda << endl;
     return renda;
 }
 //cadastro do dinheiro gasto por mês
 double cadastrar_gasto(double gasto){
     cout << "Dinheiro gasto no mês:" << endl;
     cin >> gasto;
-    cout << "Gasto mensal:" << gasto << endl;
     return gasto;
 }
 //escolha do mês
 int funcao_mes(int escolher_mes){
     cout << "Informe o mês" << endl;
     cin >> escolher_mes;
-    cout << "Mês escolhido:" << escolher_mes << endl;
     return escolher_mes;
 }
 //calculo do saldo do usuário naquele mês
 double calcular_saldo(double renda,double gasto,double saldo){
     saldo = renda - gasto;
-    cout << "Seu saldo mensal é de:" << saldo << endl;
-    return 0;
+    return saldo;
 }
-
+//mostrar informações do usuário
+void mostrar_meumes(int escolher_mes,double renda,double gasto,double saldo){
+    cout << "Mês escolhido:" << escolher_mes << endl;
+    cout << "Renda mensal:" << renda << endl;
+    cout << "Gasto mensal:" << gasto << endl;
+    cout << "Seu saldo mensal é de:" << saldo << endl;
+}
 //função principal
 int main(){
     MeuMes meumes;
-    int mes = funcao_mes(mes);
+    int escolher_mes = funcao_mes(escolher_mes);
     double renda = cadastrar_renda(renda);
     double gasto = cadastrar_gasto(gasto);
     double saldo = calcular_saldo(renda,gasto,saldo);
+    mostrar_meumes(escolher_mes,renda,gasto,saldo);
 }
