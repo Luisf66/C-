75995d3ecf87dd129699828337c00856c9f7d986
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-01 20:55:57 -0300

    vetores e ponteiros 2

--- C++.cpp
+++ C++.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 void troca_valor(int* vetor){
-    *(vetor + 0) = 3;
+    for (int i = 0; i < 10; i++){
+        int a = 5;
+        int soma = soma + a;
+        *(vetor + i) = soma;
+        cout << *(vetor + i) << endl;
+    }
 }
 
 int main(){
     int* vetor = new int[10];
-
     troca_valor(vetor);
-    cout << *(vetor + 0) << endl;
 }
