17b2fd58b52fc1c5625ad1a151606250738c62b6
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-08 17:09:54 -0300

    exercicio 14

--- C++.cpp
+++ C++.cpp
@@ -3,19 +3,14 @@
 using namespace std;
 
 int main(){
-    int num1,num2;
-    float num3,R1,R2,R3;
-
-    cout << "Digite um inteiro:" << endl;
-    cin >> num1;
-    cout << "Digite outro inteiro:" << endl;
-    cin >> num2;
-    cout << "Digite um real:" << endl;
-    cin >> num3;
-    R1 = (num1*2) * (num2/2);
-    R2 = (num1*3) + num3;
-    R3 = num3*num3*num3;
-    cout << "Resultado 1: " << R1 << endl;
-    cout << "Resultado 2: " << R2 << endl;
-    cout << "Resultado 3: " << R3 << endl;
+    int const taxa = 4;
+    int peso;
+    float valor;
+    cout << "Digite o peso:" << endl;
+    cin >> peso;
+    cout << "Peso:" << peso << endl;
+    if (peso > 50){
+        valor = (peso - 50) *taxa; 
+        cout << "Valor: " << valor;
+    }
 }
