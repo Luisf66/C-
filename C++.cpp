784439c11dbd198f702838ac9c376ebda63bf841
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-08 17:29:36 -0300

    exercicio 15

--- C++.cpp
+++ C++.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 
 int main(){
-    int peso;
-    float valor;
-    cout << "Digite o peso:" << endl;
-    cin >> peso;
-    cout << "Peso:" << peso << endl;
-    if (peso > 50){
-        valor = (peso - 50) * 4; 
-        cout << "Valor: " << valor;
-    }
+    float valor_h,salarioB,salarioL;
+    int hora_t;
+    float taxa1,taxa2,taxa3;
+
+    cout <<"Ganho por hora:"<< endl;
+    cin >> valor_h;
+    cout <<"Hora trabalhada:"<< endl;
+    cin >> hora_t;
+    salarioB = valor_h * hora_t;
+
+    taxa1 = (salarioB *11)/100;
+    taxa2 =  (salarioB *8)/100;
+    taxa3 =  (salarioB *5)/100;
+    salarioL = salarioB - taxa1 - taxa2 - taxa3;
+
+    cout << "Salario bruto:" << salarioB << endl;
+    cout << "Salario liquido:" << salarioL << endl;
+
+
 }
