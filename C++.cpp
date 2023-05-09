d3eb0d5f58928ddd3d60aa15b427a9cd8d98ccaa
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-09 20:14:17 -0300

    18

--- C++.cpp
+++ C++.cpp
@@ -4,19 +4,12 @@ using namespace std;
 
 
 int main(){
-    float area,lata,gastoL,gastoG,galao;
-    // 1 l = 6 m 
-    // 1 lata = 18 l = 80 R$ = 108 m^2
-    // 1 GALÃO = 3,6 L = 25 R$ = 21,6 m^2
+    float tamanhoMB,velocidadeMB,tempo;
 
-    cout <<"Area a ser pintada:"<< endl;
-    cin >> area;
-    lata = area / 108;
-    galao = area / 21.6;
-    gastoL = lata * 80;
-    gastoG = galao * 25;
-    cout << "Quant. latas:" << lata << endl;
-    cout << "Quant. Galao:" << galao << endl;
-    cout << "Gasto total L:" << gastoL << endl;
-    cout << "Gasto total G:" << gastoG << endl;
+    cout << "Tamanho do arquivo: " << endl;
+    cin >> tamanhoMB;
+    cout << "Velocidade da conexão: " << endl;
+    cin >> velocidadeMB;
+    tempo = (tamanhoMB * 8 ) / velocidadeMB;
+    cout << "Tempo estimado:" << tempo <<  endl;
 }
