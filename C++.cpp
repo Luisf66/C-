fc440765612c7391348de5200ee63ac1f586b81c
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-10 17:04:41 -0300

    26

--- C++.cpp
+++ C++.cpp
@@ -14,27 +14,21 @@ int main(){
     cout << "Numero 3:" << endl;
     cin >> num3;
     if (num1 > num2 && num2 > num3){
-        cout << "Numero 1 maior " << endl;
-        cout << "Numero 3 menor " << endl;
+        cout << "Produto 3 " << endl;
     }
     else if(num2 > num1 && num1 > num3){
-        cout << "Numero 2 maior " << endl;
-        cout << "Numero 3 menor " << endl;
+        cout << "Produto 3 " << endl;
     }
     else if(num3 > num1 && num1 > num2){
-        cout << "Numero 3 maior " << endl;
-        cout << "Numero 2 menor " << endl;
+        cout << "Produto 2 " << endl;
     }
     else if(num1 > num3 && num3 > num2){
-        cout << "Numero 1 maior " << endl;
-        cout << "Numero 2 menor " << endl;
+        cout << "Produto 2 " << endl;
     }
     else if(num2 > num3 && num3 > num1){
-        cout << "Numero 2 maior " << endl;
-        cout << "Numero 1 menor " << endl;
+        cout << "Produto 1 " << endl;
     }
     else if(num3 > num2 && num2 > num1){
-        cout << "Numero 3 maior " << endl;
-        cout << "Numero 1 menor " << endl;
+        cout << "Produto 1 " << endl;
     }
 }
