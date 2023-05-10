911c1e528c221c039661f9f878af8816be80728e
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-10 17:47:39 -0300

    29

--- C++.cpp
+++ C++.cpp
@@ -4,19 +4,29 @@
 using namespace std;
 
 int main(){
-    string turno;
-    cout << "Turno:(M-V-N)" << endl;
-    cin >> turno;
-    if (turno == "M"){
-        cout << "Bom dia" << endl;
+    float salario,A20,A15,A10,A5;
+    
+    cout << "Salario: " << endl;
+    cin >> salario;
+    A20 = (salario * 20)/100;
+    A15 = (salario * 15)/100;
+    A10 = (salario * 10)/100;
+    A5 = (salario * 5)/100;
+
+    if(salario <= 280){
+        salario = salario + A20;
+        cout << "Salario:"<< salario;
     }
-    else if (turno == "V"){
-        cout << "Boa tarde" << endl;
+    else if (salario > 280 && salario <= 700){
+        salario = salario + A15;
+        cout << "Salario:"<< salario;
     }
-    else if (turno == "N"){
-        cout << "Boa Noite" << endl;
+    else if (salario > 700 && salario <= 1500){
+        salario = salario + A10;
+        cout << "Salario:"<< salario;
     }
-    else{
-        cout << "Invalido" << endl;
+    else if (salario > 1500){
+        salario = salario + A5;
+        cout << "Salario:"<< salario;
     }
 }
