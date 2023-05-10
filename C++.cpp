7a9556ed4a31f7ae71a588188303861faf2cc1d9
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-10 16:53:15 -0300

    23

--- C++.cpp
+++ C++.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 
 int main(){
-    int teste[] = {2,3,5,7};
-    int num;
-    cout << "Digite um numero: " << endl;
-    cin >> num;
-    for (int i = 0; i < 4; i++){
-        if (num % teste[i] == 0){
-            cout << "Normal" << endl;
-            break;
-        }
-        else{
-            cout << "Primo" << endl;
-        }
-    };
+    float nota1,nota2,media;
+
+    cout << "Nota 1:" << endl;
+    cin >> nota1;
+    cout << "Nota 2:" << endl;
+    cin >> nota2;
+    media = (nota1 + nota2) / 2;
+    cout << "Media:" << media << endl;
+    if (media == 10){
+        cout << "Aprovado com Distincao: " << endl;
+    }
+    else if(media > 7){
+        cout << "Aprovado: " <<  endl;
+    }
+    else{
+        cout << "Reprovado: " << endl;
+    }
+    
 }
