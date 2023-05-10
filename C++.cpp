6690933e31dd3ca474528450a41114f977898382
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-10 17:34:55 -0300

    28

--- C++.cpp
+++ C++.cpp
@@ -3,45 +3,20 @@
 
 using namespace std;
 
-
 int main(){
-    float num1,num2,num3;
-
-    cout << "Numero 1:" << endl;
-    cin >> num1;
-    cout << "Numero 2:" << endl;
-    cin >> num2;
-    cout << "Numero 3:" << endl;
-    cin >> num3;
-        if(num1 > num2 && num2 > num3){
-            cout << "Numero 1:" << endl;
-            cout << "Numero 2:" << endl;
-            cout << "Numero 3:" << endl;
-        }
-        else if(num2 > num1 && num1 > num3){
-            cout << "Numero 2:" << endl;
-            cout << "Numero 1:" << endl;
-            cout << "Numero 3:" << endl;
-        }
-        else if(num2 > num3 && num3 > num1){
-            cout << "Numero 2:" << endl;
-            cout << "Numero 3:" << endl;
-            cout << "Numero 1:" << endl;
-        }
-        else if(num3 > num2 && num2 > num1){
-            cout << "Numero 3:" << endl;
-            cout << "Numero 2:" << endl;
-            cout << "Numero 1:" << endl;
-        }
-        else if(num3 > num1 && num1 > num2){
-            cout << "Numero 3:" << endl;
-            cout << "Numero 1:" << endl;
-            cout << "Numero 2:" << endl;
-        }
-        else if(num1 > num2 && num3 > num2){
-            cout << "Numero 1:" << endl;
-            cout << "Numero 3:" << endl;
-            cout << "Numero 2:" << endl;
-        }
-    
+    string turno;
+    cout << "Turno:(M-V-N)" << endl;
+    cin >> turno;
+    if (turno == "M"){
+        cout << "Bom dia" << endl;
+    }
+    else if (turno == "V"){
+        cout << "Boa tarde" << endl;
+    }
+    else if (turno == "N"){
+        cout << "Boa Noite" << endl;
+    }
+    else{
+        cout << "Invalido" << endl;
+    }
 }
