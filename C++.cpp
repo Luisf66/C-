d70701383142da98732d56b9ded20cf172cc4392
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-12 19:53:26 -0300

    31

--- C++.cpp
+++ C++.cpp
@@ -4,23 +4,16 @@
 using namespace std;
 
 int main(){
-    float hora_t,valor_t,salario,IR,INSS,FGTS;
-    
-    cout << "Horas trabalhada: " << endl;
-    cin >> hora_t;
-    cout << "Valor ganho por hora: " << endl;
-    cin >> valor_t;
-    salario = valor_t * hora_t;
-    cout << "Salario Bruto: " << salario << endl;
-    
-    FGTS = (salario * 11)/100;
-    INSS = (salario * 10)/100;
-    IR = (salario * 5)/100;
+    int num;
+    string dias[] = {"INVÁLIDO","Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"};
 
-    cout << "FGTS: " << FGTS << endl;
-    cout << "INSS: " << INSS << endl;
-    cout << "IR: " << IR << endl;
-
-    salario = salario - FGTS - INSS - IR;
-    cout << "Salario Liquido: " << salario << endl;
+    for (int i = 0; i < 7; i++){
+        cout << "Digite um numero: " << endl;
+        cin >> num;
+        if (num > 7){
+            cout << "Dia: INVÁLIDO" << endl;
+        }
+        cout << "Dia: " << dias[num] << endl;
+    }
+    
 }
