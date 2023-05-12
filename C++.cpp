1c50be6be14d46479cd494032fdc8710e7d33054
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-12 20:33:54 -0300

    35

--- C++.cpp
+++ C++.cpp
@@ -5,36 +5,15 @@
 using namespace std;
 
 int main(){
-    int a,b,c;
-    float delta,raizp,raizn;
+    int ano;
 
-    cout << "Digite o primeiro valor: " << endl;
-    cin >> a;
-    if (a == 0){
-        cout << "A equação não é de segundo grau" << endl;
-        exit(0);
-    }
+    cout << "Digite o ano: " << endl;
+    cin >> ano;
 
-    cout << "Digite o segundo valor: " << endl;
-    cin >> b;
-
-    cout << "Digite o terceiro valor: " << endl;
-    cin >> c;
-    
-    delta = (b*b) - (4 * a * c);
-    if (delta < 0){
-        cout << "A equação não possui raizes" << endl;
-        exit(0);
+    if ((ano % 4 == 0) && (ano % 100 != 0) || (ano % 400 == 0)){
+        cout << "BISSEXTO" << endl;
     }
-    else if (delta == 0){
-        cout << "A equação possui um raiz" << endl;
+    else{
+        cout << "NORMAL" << endl;
     }
-    else if (delta > 0){
-        cout << "A equação possui duas raiz" << endl;
-    }
-    raizp = (-b) + (sqrt(delta)) / (2 * a);
-    raizn = (-b) - (sqrt(delta)) / (2 * a);
-    cout << "DELTA: " << delta << endl;
-    cout << "RAIZ +: " << raizp << endl;
-    cout << "RAIZ -: " << raizn << endl;
 }
