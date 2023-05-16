b5cd6ea0dae32a003e8640f6c43a08ae1da2205f
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-15 21:27:16 -0300

    42

--- C++.cpp
+++ C++.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 
 int main(){
-    float valor;
-
-    cout << "VALOR: " << endl;
-    cin >> valor;
-
-    if (floor(valor) == valor){
-        cout << "INTEIRO" << endl;
+    string pergunta[] = {"Telefonou para a vítima?","Esteve no local do crime?","Mora perto da vítima?","Devia para a vítima?","Já trabalhou com a vítima?"};
+    int soma = 0;
+    for (int i = 0; i < 5; i++){
+        int resposta = 0;
+        cout << pergunta[i] << endl;
+        cout << "Resposta: " << endl;
+        cin >> resposta;
+        soma += resposta ;
     }
-    else {
-        cout << "DECIMAL" << endl;
+    cout << "Soma das respostas: " << soma << endl;
+    if (soma == 2){
+        cout << "Suspeito" << endl;
     }
+    else if (soma == 3 || soma == 4){
+        cout << "Cumplice" << endl;
+    }
+    else if (soma == 5){
+        cout << "Assassino" << endl;
+    }
+    else{
+        cout << "Inocente" << endl;
+    }
+
 }
