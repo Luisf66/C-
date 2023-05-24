f1e38b29a09af64c205cc38dd4356d14d98f313e
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-24 20:03:19 -0300

    46

--- C++.cpp
+++ C++.cpp
@@ -4,38 +4,15 @@
 
 using namespace std;
 
-int main(){
-    float quantidade_morango,quantidade_maca;
-    float valor_morango,valor_maca;
+/*Faça um programa que peça uma nota, entre zero e dez
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.*/
 
-    cout << "Quantidade de Morangos em Kg: " << endl;
-    cin >> quantidade_morango;
-
-    cout << "Quantidade de Maca em Kg: " << endl;
-    cin >> quantidade_maca;
-
-    if (quantidade_morango > 5)
+int main()
+{
+    int num;
+    do
     {
-        valor_morango = quantidade_morango * 2.2;
-    }
-    else
-    {
-        valor_morango = quantidade_morango * 2.5;
-    }
-
-    if (quantidade_maca > 5)
-    {
-        valor_maca = quantidade_maca * 1.5;
-    }
-    else
-    {
-        valor_maca = quantidade_maca * 1.8;
-    }
-
-    if ((quantidade_maca + quantidade_morango) > 8 || (valor_maca + valor_morango) > 25)
-    {
-        valor_maca = valor_maca - ((valor_maca * 10) / 100);
-        valor_morango = valor_morango - ((valor_morango * 10) / 100);
-        cout << "Valor Total: " << valor_morango + valor_maca << endl;
-    }
+    cout << "Insira um numero entre 0 e 10: " << endl;
+    cin >> num;
+    } while (num < 0 || num > 10);
 }
