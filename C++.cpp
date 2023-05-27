5bcb3efed16feefd3f123a6320bec90a6b992eff
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-26 21:29:16 -0300

    51

--- C++.cpp
+++ C++.cpp
@@ -5,24 +5,9 @@
 using namespace std;
 
 int main()
-{
-    float anos,popA,popB,taxaA,taxaB;
-
-    cout << "Populacao da cidade A: " << endl;
-    scanf("%f", &popA);
-    cout << "Populacao da cidade B: " << endl;
-    scanf("%f", &popB);
-    cout << "Taxa de crescimento A: " << endl;
-    scanf("%f", &taxaA);
-    cout << "Taxa de crescimento B: " << endl;
-    scanf("%f", &taxaB);
-    while (popA < popB)
+{   
+    for (int i = 1; i < 21; i++)
     {
-        popA = popA + (popA * taxaA) / 100; 
-        popB = popB + (popB * taxaB) / 100;
-        anos ++;
+        cout << i << " ";
     }
-    cout << "Anos: " << anos << endl;
-    cout << "Populacao de A:" << popA << endl;
-    cout << "Populacao de B:" << popB << endl;
 }
