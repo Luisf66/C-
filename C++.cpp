bdcac332bd27f6e5ebe8e28163c4d28ffde56430
Luisf66 <112034357+Luisf66@users.noreply.github.com>
2023-05-27 15:32:25 -0300

    55

--- C++.cpp
+++ C++.cpp
@@ -6,9 +6,15 @@ using namespace std;
 
 int main()
 {   
-    for (int i = 0; i < 50; i++)
+    int num1,num2;
+
+    cout << "Numero 1: " << endl;
+    cin >> num1;
+    cout << "Numero 2: " << endl;
+    cin >> num2;
+
+    for (int i = num1 + 1; i < num2; i++)
     {
-        i++;
         cout << i << endl;
     }
 }
