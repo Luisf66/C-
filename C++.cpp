51ecdb67a3e52651b61b18444a6ae6186f4f077f
Luis <Luisf311220@gmail.com>
2024-03-04 17:01:42 -0300

    Modularização

--- C++.cpp
+++ C++.cpp
@@ -1,17 +1,14 @@
 #include <bits/stdc++.h>
+#include "c++.h"
 using namespace std;
 
 int main()
 {
-    //uso de ponteiros
+    //modularização
+    double number_one = 9.8;
+    double number_two = 3.4;
+    
+    cout << soma(number_one, number_two);
 
-    int numero = 0;
-    cout << "Digite o número: " << endl;
-    cin >> numero;
-    cout << "O valor da variável guardado foi: " << numero << endl;
-
-    int* apontador_numero;
-    apontador_numero = &numero;
-    cout << "O valor do apontador guardado foi: " << *(static_cast<int*>(apontador_numero)) << endl;
-    cout << "O valor do apontador é: " << apontador_numero << endl;
+    cout << "\nFim do Programa" << endl;
 }
---
+++ C++.h
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double soma (double a, double b) {
+    return a + b;
+}
