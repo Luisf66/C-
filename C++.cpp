aed63c16efea3acc6b981ff2fa3df9cafd4c829d
Luis <Luisf311220@gmail.com>
2024-03-08 11:29:24 -0300

    Herança

--- C++.cpp
+++ C++.cpp
@@ -3,20 +3,41 @@ using namespace std;
 
 int main()
 {
-    //uso de classes
-    class Linguagem {
+    // uso de classes
+    class Estudante
+    {
+    private:
         string nome;
-        int ano_lancamento;
+        string curso;
+        int idade = 0;
 
-        public:
-        void mensagem(string name, int ano){
-            nome = name;
-            ano_lancamento = ano;
-            cout << "A linguagem " << nome << " foi criada em " << ano_lancamento << endl;
-        };
+    public:
+        void Preencher_dados()
+        {
+            cout << "Nome: " << endl;
+            cin >> nome;
+            cout << "Curso: " << endl;
+            cin >> curso;
+            cout << "Idade: " << endl;
+            cin >> idade;
+        }
+        void Exibir_dados()
+        {
+            cout << nome << " estuda no curso de " << curso << " e tem " << idade << " anos";
+        }
     };
-
-    Linguagem cplus;
-    cplus.mensagem("C++", 1979);
-
+    //-------------------------------------------------------------------------------------------------------//
+    class Trabalhador : public Estudante
+    {
+    public:
+        void Trabalho()
+        {
+            cout << " e tambem trabalha" << endl;
+        }
+    };
+    //-------------------------------------------------------------------------------------------------------//
+    Trabalhador aluno;
+    aluno.Preencher_dados();
+    aluno.Exibir_dados();
+    aluno.Trabalho();
 }
