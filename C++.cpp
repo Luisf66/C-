e572405c1bb4a7ca5d71a982de30cb3830d45b80
Luisf66 <Luisf311220@gmail.com>
2024-03-23 20:47:16 -0300

    Namespace

--- C++.cpp
+++ C++.cpp
@@ -1,43 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+// Uso do namespace
+namespace Funcao {
+    int somar(int a, int b) {
+        int resultado = a + b;
+        cout << "Resultdo: " << resultado;
+        return resultado; 
+    }
+}
 
 int main()
 {
-    // uso de classes
-    class Estudante
-    {
-    private:
-        string nome;
-        string curso;
-        int idade = 0;
-
-    public:
-        void Preencher_dados()
-        {
-            cout << "Nome: " << endl;
-            cin >> nome;
-            cout << "Curso: " << endl;
-            cin >> curso;
-            cout << "Idade: " << endl;
-            cin >> idade;
-        }
-        void Exibir_dados()
-        {
-            cout << nome << " estuda no curso de " << curso << " e tem " << idade << " anos";
-        }
-    };
-    //-------------------------------------------------------------------------------------------------------//
-    class Trabalhador : public Estudante
-    {
-    public:
-        void Trabalho()
-        {
-            cout << " e tambem trabalha" << endl;
-        }
-    };
-    //-------------------------------------------------------------------------------------------------------//
-    Trabalhador aluno;
-    aluno.Preencher_dados();
-    aluno.Exibir_dados();
-    aluno.Trabalho();
+    int valor1, valor2 = 0;
+    cout << "Digite um valor: " << endl;
+    cin >> valor1;
+    cout << "Digite outro valor: " << endl;
+    cin >> valor2;
+    Funcao::somar(valor1,valor2);
 }
--- C++.h
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-double soma (double a, double b) {
-    return a + b;
-}
