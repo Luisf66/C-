ae81f570207a8da13c95cc3d6a6a5b555a9d7487
Luis <Luisf311220@gmail.com>
2024-04-20 20:49:05 -0300

    Polimorfismo dinamico

--- C++.cpp
+++ C++.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 using namespace std;
 
-// Uso do namespace
-namespace Funcao {
-    int somar(int a, int b) {
-        int resultado = a + b;
-        cout << "Resultdo: " << resultado;
-        return resultado; 
+class Polimorfismo
+{
+public:
+    virtual void Dinamico()
+    {
+        cout << "Sou um método dinamico e posso ser sobrescrito" << endl;
     }
-}
+};
+
+class Sobrescreve : public Polimorfismo
+{
+public:
+    void Dinamico() override
+    {
+        cout << "Sobrescrevi o método Dinamico da classe Pai" << endl;
+    }
+};
 
 int main()
 {
-    int valor1, valor2 = 0;
-    cout << "Digite um valor: " << endl;
-    cin >> valor1;
-    cout << "Digite outro valor: " << endl;
-    cin >> valor2;
-    Funcao::somar(valor1,valor2);
+    Polimorfismo *poli;
+    Polimorfismo pai;
+    Sobrescreve sobre;
+
+    pai.Dinamico();
+
+    poli = &sobre;
+    poli->Dinamico();
+
+    return 0;
 }
