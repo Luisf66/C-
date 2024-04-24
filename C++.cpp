040c3efad64ae43fb4fd9a66d49093deef21657a
Luis <Luisf311220@gmail.com>
2024-04-24 16:20:37 -0300

    Implementação de Pilha

--- C++.cpp
+++ C++.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <stack>
 using namespace std;
-
-//PROTOTIPO
-    int fatorial(int num);
-//PROTOTIPO
 
 int main()
 {
-    int fat = 0;
-    fat = fatorial(3);
-    cout << fat << endl;
+    int escolha = 0;      // variável para seleção das opções
+    stack<string> pratos; // pilha de prato
+    do
+    {
+        cout << "Escolha entre:" << endl;
+        cout << "1- Adicionar Prato" << endl;
+        cout << "2- Remover Prato" << endl;
+        cout << "3- Ver tamanho da pilha" << endl;
+        cout << "4- Verificar pilha vazia" << endl;
+        cin >> escolha;
+
+        switch (escolha)
+        {
+        case 1:
+            pratos.push("Prato Adicionado");
+            cout << "Prato Adicionado" << endl;
+            break;
+        case 2:
+            pratos.pop();
+            cout << "Prato Removido" << endl;
+            break;
+        case 3:
+            cout << "Tamanho da Pilha: ";
+            cout << pratos.size();
+            break;
+        case 4:
+            !pratos.empty() ? cout << "Pilha com Prato\n" : cout << "Pilha vazia\n";
+            break;
+        default:
+            break;
+        }
+    } while (escolha != 0);
     return 0;
 }
-
-// FATORIAL
-int fatorial(int num) 
-{
-    return (num == 0) ?  1 :  num * fatorial(num - 1);  
-}
-// FATORIAL
